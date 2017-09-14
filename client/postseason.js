0fc0b84502f2b9a6050a020a4631a7ca170b9702
Trenton Pottruff <jayitinc@gmail.com>
2017-09-14 12:04:37 -0400

    All Divisions

--- client/postseason.js
+++ client/postseason.js
@@ -60,6 +60,90 @@ Template.postseason.helpers({
       }
     });
   },
+  'alw': function() {
+    return Standings.find({league: "American", division: "West"}, {
+      transform: function(item){
+        item.avg = (item.wins/(item.wins+item.losses));
+
+        var top = Standings.find({league: "American", division: "West"}, {sort: {
+          wins: -1,
+          losses: 1
+        }}).fetch()[0];
+
+        if (top.name == item.name) {
+          item.elimination = "-";
+        } else {
+          item.elimination = 163 - top.wins - item.losses;
+
+          if (item.elimination <= 0) {
+            item.elimination = "E";
+          }
+        }
+
+        return item;
+      },
+      sort: {
+        wins: -1,
+        losses: 1
+      }
+    });
+  },
+  'nlc': function() {
+    return Standings.find({league: "National", division: "Central"}, {
+      transform: function(item){
+        item.avg = (item.wins/(item.wins+item.losses));
+
+        var top = Standings.find({league: "National", division: "Central"}, {sort: {
+          wins: -1,
+          losses: 1
+        }}).fetch()[0];
+
+        if (top.name == item.name) {
+          item.elimination = "-";
+        } else {
+          item.elimination = 163 - top.wins - item.losses;
+
+          if (item.elimination <= 0) {
+            item.elimination = "E";
+          }
+        }
+
+        return item;
+      },
+      sort: {
+        wins: -1,
+        losses: 1
+      }
+    });
+  },
+  'nlw': function() {
+    return Standings.find({league: "National", division: "West"}, {
+      transform: function(item){
+        item.avg = (item.wins/(item.wins+item.losses));
+
+        var top = Standings.find({league: "National", division: "West"}, {sort: {
+          wins: -1,
+          losses: 1
+        }}).fetch()[0];
+
+        if (top.name == item.name) {
+          item.elimination = "-";
+        } else {
+          item.elimination = 163 - top.wins - item.losses;
+
+          if (item.elimination <= 0) {
+            item.elimination = "E";
+          }
+        }
+
+        return item;
+      },
+      sort: {
+        wins: -1,
+        losses: 1
+      }
+    });
+  },
   'alecount': function() {
     return Standings.find().count();
   },
