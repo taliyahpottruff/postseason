2e2e103e283b0a428bcd7be48df107b9f4c00678
Trenton Pottruff <jayitinc@gmail.com>
2017-09-23 20:01:08 -0400

    Updated seeding

--- client/postseason.js
+++ client/postseason.js
@@ -144,6 +144,66 @@ Template.postseason.helpers({
       }
     });
   },
+  'alwc': function() {
+    return Standings.find({league: "American", division: "Wildcard"}, {
+      transform: function(item){
+        item.avg = (item.wins/(item.wins+item.losses));
+
+        var query = Standings.find({league: "American", division: "Wildcard"}, {sort: {
+          wins: -1,
+          losses: 1
+        }}).fetch();
+        var top = query[0];
+        var runnerup = query[1];
+
+        if (top.name == item.name || runnerup.name == item.name) {
+          item.elimination = "-";
+        } else {
+          item.elimination = 163 - runnerup.wins - item.losses;
+
+          if (item.elimination <= 0) {
+            item.elimination = "E";
+          }
+        }
+
+        return item;
+      },
+      sort: {
+        wins: -1,
+        losses: 1
+      }
+    });
+  },
+  'nlwc': function() {
+    return Standings.find({league: "National", division: "Wildcard"}, {
+      transform: function(item){
+        item.avg = (item.wins/(item.wins+item.losses));
+
+        var query = Standings.find({league: "National", division: "Wildcard"}, {sort: {
+          wins: -1,
+          losses: 1
+        }}).fetch();
+        var top = query[0];
+        var runnerup = query[1];
+
+        if (top.name == item.name || runnerup.name == item.name) {
+          item.elimination = "-";
+        } else {
+          item.elimination = 163 - runnerup.wins - item.losses;
+
+          if (item.elimination <= 0) {
+            item.elimination = "E";
+          }
+        }
+
+        return item;
+      },
+      sort: {
+        wins: -1,
+        losses: 1
+      }
+    });
+  },
   'alecount': function() {
     return Standings.find().count();
   },
